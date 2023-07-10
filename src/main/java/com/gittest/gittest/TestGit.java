8a30a753f78de888e43c0d0b558c60fb502b7db6
luoyuhao <506205644@qq.com>
2023-07-10 10:51:20 +0800

    master commit

--- src/main/java/com/gittest/gittest/TestGit.java
+++ src/main/java/com/gittest/gittest/TestGit.java
@@ -2,6 +2,6 @@ package com.gittest.gittest;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("master");
+        System.out.println("master2");
     }
 }
