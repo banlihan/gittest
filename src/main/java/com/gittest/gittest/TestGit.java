c663be5cd0bd622cb4241dec396d069d394070b3
luoyuhao <506205644@qq.com>
2023-07-10 11:32:48 +0800

    master commit3

--- src/main/java/com/gittest/gittest/TestGit.java
+++ src/main/java/com/gittest/gittest/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("master");
         System.out.println("hot-fox");
+        System.out.println("hot-fix");
     }
 }
