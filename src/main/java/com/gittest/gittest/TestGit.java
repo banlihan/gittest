c1de58c67daa75ffce2568def60fc5e30c0d7ed5
luoyuhao <506205644@qq.com>
2023-07-10 11:28:36 +0800

    hot-fix commit2

--- src/main/java/com/gittest/gittest/TestGit.java
+++ src/main/java/com/gittest/gittest/TestGit.java
@@ -3,6 +3,6 @@ package com.gittest.gittest;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("master");
-        System.out.println("hot-fix");
+        System.out.println("hot-fox");
     }
 }
