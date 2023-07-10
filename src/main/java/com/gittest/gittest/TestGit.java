3d899b8ec7815b7e8bb8291f2ac9e623a9e80dd8
luoyuhao <506205644@qq.com>
2023-07-10 23:41:00 +0800

    Merge branch 'hot-fix'
    # Conflicts:
    #	src/main/java/com/gittest/gittest/TestGit.java

--- src/main/java/com/gittest/gittest/TestGit.java
+++ src/main/java/com/gittest/gittest/TestGit.java
@@ -8,5 +8,6 @@ public class TestGit {
         System.out.println("master");
         System.out.println("master2");
         System.out.println("hot-fix2");
+        System.out.println("push test");
     }
 }
